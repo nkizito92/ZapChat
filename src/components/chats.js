b3b31a0608bbb50badf527a6deb4fa4d2c8fa521
nkizito92 <nkizito57@gmail.com>
2020-04-15 09:03:13 -0500

    Capitalized name of components

--- src/components/chats.js
+++ src/components/chats.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class Chats extends Component {
-    render (){
+    render() {
         return (
             <div>
                 Stop
