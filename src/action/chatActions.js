6bca8fe06ad0c99fb129d12df4d84ab230705bb6
nkizito92 <nkizito57@gmail.com>
2020-04-16 12:07:27 -0500

    added createChat for posting

--- src/action/chatActions.js
+++ src/action/chatActions.js
@@ -1,12 +1,39 @@
+export const addChat = (chat) => {
+    return {
+        type: 'ADD_CHAT',
+        chat: chat
+    }
+}
 export const fetchChats = () => {
     return (dispatch) => {
         dispatch({ type: 'LOADING_CHATS' })
         fetch('http://localhost:3000/chats').then(res => {
             return res.json()
         }).then(guessChat => {
-            dispatch({ type: 'ADD_CHATS', chats: guessChat.message })
-            console.log(guessChat.message)
+            const messages = guessChat.map(chat => chat.message)
+                dispatch({ type: 'ADD_CHATS', chats: guessChat})
+            console.log(messages)
         })
     }
 
+}
+
+export const createChat = (chat) => {
+    return (dispatch) => {
+        
+        fetch('http://localhost:3000/chats', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({chat: chat})
+        } 
+        
+        )
+        .then(res => res.json())
+        .then(chat => dispatch(addChat(chat)))
+        .catch(error => {
+            console.log(error)
+        })
+    }
 }
