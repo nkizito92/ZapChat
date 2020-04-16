7bc955ace98d8834dedfd4ec7ac47d7310ca3824
nkizito92 <nkizito57@gmail.com>
2020-04-16 12:09:27 -0500

    added the Chats and ChatsInput components

---
+++ src/container/ChatsContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Chats from "../components/Chats"
+// import Chat from "../components/Chat"
+import { fetchChats } from "../action/chatActions"
+import ChatsInput from "../components/ChatsInput"
+
+class ChatsContainer extends Component {
+    componentDidMount() {
+        this.props.fetchChats()
+    }
+
+    render() {
+        return (
+            <div>
+                <ChatsInput />
+                <Chats />
+                {/* <Chat /> */}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        chats: state.chats,
+        loading: state.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchChats: () => dispatch(fetchChats())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatsContainer)
