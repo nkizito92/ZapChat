0678b712a88d01ea857ddd9a6f9614756ca63af8
nkizito92 <nkizito57@gmail.com>
2020-04-18 11:36:09 -0500

    added a guestsReducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import Chats from './components/Chats'
+import Guests from './components/guestComponents/Guests'
+import ChatsInput from './components/ChatsInput'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import chatsReducer from './reducers/chatsReducer'
+import guestsReducer from './reducers/guestsReducer'
 import thunk from 'redux-thunk'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './headers/Navbar'
 
-const store = createStore(chatsReducer, applyMiddleware(thunk))
+const store = createStore(chatsReducer, guestsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
+    <Router>
+        <nav>
+            <Navbar />
+        </nav>
+            <Route exact path="/" component={App} />
+            <Route exact path="/chats" component={Chats} />
+            <Route exact path="/chats/new" component={ChatsInput} />
+            <Route exact path="/guests" component={Guests} />
+    </Router>
 
-    <App />
     </Provider>,
     document.getElementById('root')
 );
