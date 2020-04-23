2b4a8e88645aa62651190f7b6a24a8a93cf9f343
nkizito92 <nkizito57@gmail.com>
2020-04-22 21:39:24 -0500

    editting style

--- src/headers/Navbar.js
+++ src/headers/Navbar.js
@@ -5,19 +5,18 @@ const link = {
     padding: "8px 10px",
     margin: "0 3px",
     color: "#ffff",
-    backgroundColor: 'black',
     textDecoration: 'none'
 }
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <NavLink to="/" exact style={link} activeStyle={{ backgroundColor: "gray" }}>Home</NavLink>
-                <NavLink to="/chats" exact style={link} activeStyle={{ backgroundColor: "gray" }}>Chats</NavLink>
-                <NavLink to="/chats/new" exact style={link} activeStyle={{ backgroundColor: "gray" }}>NewChat</NavLink>
-                <NavLink to="/guests" exact style={link} activeStyle={{ backgroundColor: "gray" }}>Guests</NavLink>
-           </div>
+            <>
+                <NavLink to="/" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Home</NavLink>
+                <NavLink to="/chats" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Chats</NavLink>
+                <NavLink to="/chats/new" exact style={link} activeStyle={{ backgroundColor: "navy" }}>NewChat</NavLink>
+                <NavLink to="/guests" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Guests</NavLink>
+           </>
         )
     }
 }
