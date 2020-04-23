93d908f6ef6bf52a7bbe4a87fd28917f596d586b
nkizito92 <nkizito57@gmail.com>
2020-04-22 23:50:33 -0500

    setting props for components and changed routes for container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Home from './components/Home'
 import ChatsContainer from './container/ChatsContainer'
-import Chats from './components/chatComponents/Chats'
 import ChatShow from './components/chatComponents/ChatShow'
-import CommentsContainer from './container/CommentsContainer'
 import CommentShow from './components/commentComponents/CommentShow'
-import Guests from './components/guestComponents/Guests'
+import GuestsContainer from './container/GuestsContainer'
 import ChatsInput from './components/chatComponents/ChatsInput'
 import { Route, Switch } from 'react-router-dom'
 import Navbar from './headers/Navbar'
@@ -33,13 +32,12 @@ export default class App extends React.Component {
                     <br />
                 </nav>
                 <Switch>
-                    <Route exact path="/" component={ChatsContainer} />
-                    <Route exact path="/chats" component={Chats} />
+                    <Route exact path="/" component={() => <Home chats={this.state.chats} />} />
+                    <Route exact path="/chats" component={ChatsContainer} />
                     <Route exact path="/chats/new" render={() => <ChatsInput />} />
                     <Route exact path="/chats/:id" component={({ match, history }) => <ChatShow chats={this.state.chats} id={match.params.id} history={history} />} />
                     <Route exact path="/comments/:id" component={({ match }) => <CommentShow comments={this.state.comments} id={match.params.id} />} />
-                    <Route exact path="/guests" component={Guests} />
-                    <Route exact path="/comments" component={CommentsContainer} />
+                    <Route exact path="/guests" component={GuestsContainer} />
                 </Switch>
             </div>
         )
