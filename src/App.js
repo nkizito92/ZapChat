7f7f60df318bea4125cc9d6910da308e1fc2e765
nkizito92 <nkizito57@gmail.com>
2020-04-24 12:22:57 -0500

    Change Home To HomesContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Home from './components/Home'
+import HomesContainer from './container/HomesContainer'
 import ChatsContainer from './container/ChatsContainer'
 import ChatShow from './components/chatComponents/ChatShow'
 import CommentShow from './components/commentComponents/CommentShow'
 import GuestsContainer from './container/GuestsContainer'
-import ChatsInput from './components/chatComponents/ChatsInput'
+import ChatsInput from './components/chatComponents/ChatsInput' 
 import { Route, Switch } from 'react-router-dom'
 import Navbar from './headers/Navbar'
 import "./style.css"
 
 
-export default class App extends React.Component {
+class App extends React.Component {
     componentDidMount() {
         fetch("http://localhost:3000/chats")
             .then(res => res.json())
@@ -26,21 +26,24 @@ export default class App extends React.Component {
     }
     render() {
         return (
-            <div className="App">
+            <div>
                 <nav id="nav">
                     <Navbar />
                     <br />
                 </nav>
                 <Switch>
-                    <Route exact path="/" component={() => <Home chats={this.state.chats} />} />
+                    <Route exact path="/" component={HomesContainer} />
                     <Route exact path="/chats" component={ChatsContainer} />
                     <Route exact path="/chats/new" render={() => <ChatsInput />} />
                     <Route exact path="/chats/:id" component={({ match, history }) => <ChatShow chats={this.state.chats} id={match.params.id} history={history} />} />
                     <Route exact path="/comments/:id" component={({ match }) => <CommentShow comments={this.state.comments} id={match.params.id} />} />
                     <Route exact path="/guests" component={GuestsContainer} />
                 </Switch>
+                <footer>Copy Rights Reserved by Kizito Njoku</footer>
             </div>
         )
     }
 
 }
+
+export default App
