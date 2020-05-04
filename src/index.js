b074c3a73f17956a4ee8b90303ae48f0cce895c2
nkizito92 <nkizito57@gmail.com>
2020-05-04 12:04:49 -0500

    Add reducx devtools to view on the web browser

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom'
 import thunk from 'redux-thunk'
 import combineReducers from './reducers/index'
-
-const store = createStore(combineReducers, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(combineReducers, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 ReactDOM.render(
     <Provider store={store} >
             <Router>
