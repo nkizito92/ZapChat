227772699146524e6692d3f9deb016f80f318701
nkizito92 <nkizito57@gmail.com>
2020-05-15 08:51:40 -0500

    users fetch and login route has been added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomesContainer from './container/HomesContainer'
 import ChatsContainer from './container/ChatsContainer'
+import Login from './components/accountComponents/Login'
 import GuestsContainer from './container/GuestsContainer'
 import ChatShow from './components/chatComponents/ChatShow'
 import CommentShow from './components/commentComponents/CommentShow'
@@ -8,6 +9,7 @@ import ChatsInput from './components/chatComponents/ChatsInput'
 import { fetchChats } from './action/chatActions'
 import { fetchComments } from './action/commentActions'
 import { fetchGuests } from './action/guestActions'
+import { fetchUsers} from './action/userActions'
 import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import Navbar from './headers/Navbar'
@@ -16,25 +18,31 @@ import "./style.css"
 
 class App extends React.Component {
     componentDidMount() {
-            this.props.fetchComments()
-            this.props.fetchChats()
-            this.props.fetchGuests()
+        this.props.fetchComments()
+        this.props.fetchChats()
+        this.props.fetchGuests()
+        this.props.fetchUsers()
+    }
+
+    displayLogin () {
+       
     }
 
     render() {
         return (
             <div>
-                <nav id="nav"> 
+                <nav id="nav">
                     <Navbar />
                     <br />
                 </nav>
                 <Switch>
-                    <Route exact path="/" component={() => <HomesContainer guests={this.props.guests} chats={this.props.chats}/>} />
-                    <Route exact path="/chats" component={() => <ChatsContainer guests={this.props.guests} chats={this.props.chats}/>} />
+                    <Route exact path="/" component={() => <HomesContainer guests={this.props.guests} chats={this.props.chats} />} />
+                    <Route exact path="/chats" component={() => <ChatsContainer guests={this.props.guests} chats={this.props.chats} />} />
                     <Route exact path="/chats/new" component={({ history }) => <ChatsInput history={history} />} />
                     <Route exact path="/chats/:id" component={({ match, history }) => <ChatShow chats={this.props.chats} id={Number(parseInt(match.params.id))} history={history} />} />
                     <Route exact path="/comments/:id" component={({ match, history }) => <CommentShow history={history} comments={this.props.comments} id={match.params.id} />} />
-                    <Route exact path="/guests" component={({history}) => <GuestsContainer history={history} chats={this.props.chats} guests={this.props.guests}/>} />
+                    <Route exact path="/guests" component={({ history }) => <GuestsContainer history={history} chats={this.props.chats} guests={this.props.guests} />} />
+                    <Route exact path="/login" component={({ history }) => <Login history={history} />} />
                 </Switch>
                 <footer>Copy Rights Reserved by Kizito Njoku</footer>
             </div>
@@ -49,4 +57,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { fetchChats, fetchComments, fetchGuests })(App)
+export default connect(mapStateToProps, { fetchChats, fetchComments, fetchGuests, fetchUsers })(App)
