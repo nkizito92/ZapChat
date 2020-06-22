3599f1a333ae98ca290de791f1dffb439516aa1d
nkizito92 <nkizito57@gmail.com>
2020-06-22 14:53:53 -0500

    remove console.log(chat)

--- src/components/chatComponents/Chat.js
+++ src/components/chatComponents/Chat.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import { updateChat } from '../../action/chatActions'
 
 const Chat = ({ chat, updateChat }) => {
-    console.log(chat)
     return (
         <div className="chat">
             <h2>{chat.guest.name}</h2> < br />
@@ -12,29 +11,29 @@ const Chat = ({ chat, updateChat }) => {
             <p>{chat.message}</p>
             <Link to={"chats/" + chat.id}>View Chat</Link> <br />
             <span id="likes">{chat.like}</span> <label> Likes</label><br />
-                    <span id="likes">{chat.laugh}</span> <label> Laugh</label><br />
-                    <span id="likes">{chat.angry}</span> <label> Angry</label><br />
-                    <button onClick={() => {
-                        if (chat.like === 0) (updateChat((chat.like + 1), chat.laugh, chat.angry, chat.id))
-                        else ((updateChat(chat.like - 1, chat.laugh, chat.angry, chat.id)))
-                    }
+            <span id="likes">{chat.laugh}</span> <label> Laugh</label><br />
+            <span id="likes">{chat.angry}</span> <label> Angry</label><br />
+            <button onClick={() => {
+                if (chat.like === 0) (updateChat((chat.like + 1), chat.laugh, chat.angry, chat.id))
+                else ((updateChat(chat.like - 1, chat.laugh, chat.angry, chat.id)))
+            }
 
-                    }>Like</button>
+            }>Like</button>
 
 
-                    <button onClick={() => {
-                        if (chat.laugh === 0) (updateChat(chat.like, (chat.laugh + 1), chat.angry, chat.id))
-                        else ((updateChat(chat.like, (chat.laugh - 1), chat.angry, chat.id)))
-                    }
+            <button onClick={() => {
+                if (chat.laugh === 0) (updateChat(chat.like, (chat.laugh + 1), chat.angry, chat.id))
+                else ((updateChat(chat.like, (chat.laugh - 1), chat.angry, chat.id)))
+            }
 
-                    }>laugh</button>
+            }>laugh</button>
 
-                    <button onClick={() => {
-                        if (chat.angry === 0) (updateChat(chat.like, chat.laugh, (chat.angry + 1), chat.id))
-                        else ((updateChat(chat.like, chat.laugh, (chat.angry - 1), chat.id)))
-                    }
+            <button onClick={() => {
+                if (chat.angry === 0) (updateChat(chat.like, chat.laugh, (chat.angry + 1), chat.id))
+                else ((updateChat(chat.like, chat.laugh, (chat.angry - 1), chat.id)))
+            }
 
-                    }>angry</button>
+            }>angry</button>
 
         </div>
     )
