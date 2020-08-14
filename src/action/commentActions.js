3a5df878b42b4bda91c91b7ab41222622f525821
nkizito92 <nkizito57@gmail.com>
2020-08-14 13:30:12 -0500

    fixed typo

--- src/action/commentActions.js
+++ src/action/commentActions.js
@@ -1,92 +1,86 @@
-import { onlineUrl } from "./urlLink"
+import { onlineUrl } from "./urlLink";
 export const fetchComments = () => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_COMMENTS' })
-        fetch(`${onlineUrl}/comments`).then(res => {
-            return res.json()
-        }).then(comment => {
-            dispatch({ type: 'ADD_COMMENTS', comments: comment })
-        })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-}
+  return (dispatch) => {
+    dispatch({ type: "LOADING_COMMENTS" });
+    fetch(`${onlineUrl()}/comments`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((comment) => {
+        dispatch({ type: "ADD_COMMENTS", comments: comment });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
-export const addComment = comment => {
-    return {
-        type: "ADD_COMMENT",
-        comment: comment
-    }
-}
+export const addComment = (comment) => {
+  return {
+    type: "ADD_COMMENT",
+    comment: comment,
+  };
+};
 
-export const createComment = comment => {
-    return dispatch => {
-        fetch(`${onlineUrl}/comments`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ comment: comment })
-        }
-
-        )
-            .then(res => res.json())
-            .then(comment => dispatch(addComment(comment)))
-            .catch(error => {
-                console.log(error)
-            })
-    }
-}
-
+export const createComment = (comment) => {
+  return (dispatch) => {
+    fetch(`${onlineUrl()}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ comment: comment }),
+    })
+      .then((res) => res.json())
+      .then((comment) => dispatch(addComment(comment)))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
 export const editComment = (comment) => {
-    return {
-        type: "EDIT_COMMENT",
-        comment: comment
-    }
-}
-
+  return {
+    type: "EDIT_COMMENT",
+    comment: comment,
+  };
+};
 
 export const updateComment = (like, laugh, angry, comment) => {
-    return dispatch => {
-        fetch(`${onlineUrl}/comments/${comment.id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                comment: comment,
-                like: like,
-                laugh: laugh,
-                angry: angry
-            })
-        }
+  return (dispatch) => {
+    fetch(`${onlineUrl()}/comments/${comment.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        comment: comment,
+        like: like,
+        laugh: laugh,
+        angry: angry,
+      }),
+    })
+      .then((res) => res.json())
+      .then((comment) => dispatch(editComment(comment)))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
-        )
-            .then(res => res.json())
-            .then(comment => dispatch(editComment(comment)))
-            .catch(error => {
-                console.log(error)
-            })
-    }
-}
+export const removeComment = (id) => {
+  return {
+    type: "DELETE_COMMENT",
+    comment: id,
+  };
+};
 
-export const removeComment = id => {
-    return {
-        type: "DELETE_COMMENT",
-        comment: id
-    }
-}
-
-export const deleteComment = commentId => {
-    return (dispatch) => {
-        fetch(`${onlineUrl}/comments/${commentId}`, {
-            method: 'DELETE'
-        })
-            .then(res => res.json())
-            .then(commentId => dispatch(removeComment(commentId)))
-    }
-}
-
-
+export const deleteComment = (commentId) => {
+  return (dispatch) => {
+    fetch(`${onlineUrl()}/comments/${commentId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((commentId) => dispatch(removeComment(commentId)));
+  };
+};
