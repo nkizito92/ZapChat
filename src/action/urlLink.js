9e6aec8e7f9726ffa6649f485bf1830dddc20464
nkizito92 <nkizito57@gmail.com>
2020-08-14 13:34:40 -0500

    re-worded the variables

--- src/action/urlLink.js
+++ src/action/urlLink.js
@@ -1,6 +1,6 @@
-// let url = "http://localhost:3000"
-let oLUrl = "https://backend-zapchat.herokuapp.com"
+// let lhost = "http://localhost:3000";
+let oLUrl = "https://backend-zapchat.herokuapp.com";
 
 export const onlineUrl = () => {
-    return oLUrl
-}
+  return oLUrl;
+};
