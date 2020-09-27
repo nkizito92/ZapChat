f9e10fa0a4650eeb98f783281273af26e887f1be
nkizito92 <nkizito57@gmail.com>
2020-09-26 23:04:19 -0500

    removed comments

--- src/components/chatComponents/Chats.js
+++ src/components/chatComponents/Chats.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react'
 import Chat from "./Chat"
 class Chats extends Component {
+    state = {
+        content: this.props.chats
+    }
     displayChats = chatPost => {
         let test = chatPost[0]
         if (test !== undefined) {
-            const listChats = chatPost.map(chat =>{ 
+            const listChats = chatPost.map(chat => {
                 const guest = this.props.guests.find(g => g.id === chat.guest_id)
-           return <Chat key={chat.id} chat={chat} guest={guest} id={chat.id} />
-        })
+                return <Chat key={chat.id} chat={chat} guest={guest} id={chat.id} />
+            })
             return (
                 <div className="main">
                     {listChats}
                 </div>
             )
         } else {
-            return <div className="main"><br/><h1 className="chatShow">No Chats Posted</h1></div>
+            return <div className="main"><br /><h1 className="chatShow">No Chats Posted</h1></div>
         }
     }
     render() {
-        const { chats } = this.props
-            return (
-                <div className="main">
-                    <h1 className="chatShow">List Of Chats</h1> <br />
-                    {this.displayChats(chats)}
-                </div>
-            )
+        return (
+            <div className="main">
+                <h1 className="chatShow">List Of Chats</h1> <br />
+                {this.displayChats(this.state.content)}
+            </div>
+        )
 
     }
 }
